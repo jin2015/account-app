0e9515c2d9676a69a18de33bc128501f56dae78d
Jin Hao Li <Jin789@gmail.com>
2020-05-01 16:05:43 -0400

    udpated app with account table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AccountTable from './AccountTable'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AccountTable />
+      <div class="load">
+            <a>Load more</a>
+      </div>  
+
     </div>
   );
 }
